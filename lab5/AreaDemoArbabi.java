cd73fc9f6455e10871c30ba76e45491d240f26fd
Arvin Arbabi <arvin.arbabi50@gmail.com>
2022-08-02 09:38:21 -0700

    lab 5 assignments

---
+++ lab5/AreaDemoArbabi.java
@@ -0,0 +1,15 @@
+public class AreaDemoArbabi {
+   public static void main(String[] args) {
+
+      // Area of a circle
+      System.out.println(AreaArbabi.getArea(20.0));
+  
+      // Area of a rectangle
+      System.out.println(AreaArbabi.getArea(10, 20));
+  
+      // Area of cylinder
+      System.out.println(AreaArbabi.getArea(10.0, 15.0));
+  }
+
+  
+}
